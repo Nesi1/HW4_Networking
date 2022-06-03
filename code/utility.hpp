f2a5c7c90714d21fc7c1e8543171c1eec9743ede
EyalAtt65 <eyaleyal65@gmail.com>
2022-06-03 06:02:33 -0700

    setup in mininet

--- code/utility.hpp
+++ code/utility.hpp
@@ -9,7 +9,7 @@
 #define HOSTS_INTERFACE_ATTR "10.0.0.1"
 #define MESSAGE_LENGTH 2
 
-sockaddr_in make_sockaddr(const std::string& ipv4_addr, uint16_t port);
+sockaddr_in make_sockaddr(const std::string& addr, uint16_t port);
 
 void handle_sys_error(const std::string& error_msg);
 
